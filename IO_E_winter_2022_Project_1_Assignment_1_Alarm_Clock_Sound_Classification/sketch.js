ce07647a2c1a10bea1664c173ed85e9bad679ec8
delinay <92961046+delinay@users.noreply.github.com>
2022-01-27 19:30:06 -0500

    Add files via upload

---
+++ IO_E_winter_2022_Project_1_Assignment_1_Alarm_Clock_Sound_Classification/sketch.js
@@ -0,0 +1,59 @@
+
+// Storing the label
+let label = "waiting...";
+
+// Classifier and model url
+let classifier;
+let modelURL = 'https://teachablemachine.withgoogle.com/models/7pn03KNhq/';
+
+// STEP 1: Load the model!
+function preload() {
+  classifier = ml5.soundClassifier(modelURL + 'model.json');
+}
+
+function setup() {
+  createCanvas(640, 520);
+
+  // STEP 2: Start classifying (will listen to mic by default)
+  classifyAudio();
+}
+
+// STEP 2 classify!
+function classifyAudio() {
+  classifier.classify(gotResults);
+}
+
+function draw() {
+  background(255, 204, 255);
+
+  // STEP 4: Draw the label
+  // textSize(32);
+  textAlign(CENTER, CENTER);
+  // fill(255);
+  // text(label, width/2, height - 16);
+
+  // Background noise is headphones
+  let emoji = "✂️";
+  // Pick an emoji based on label
+  if (label == "Watch") {
+    emoji = "⌚";
+  } else if (label == "Alarm Clock") {
+    emoji = "⏰";
+  } else if (label == "Haircut") {
+    emoji = "💇‍♀️";
+  }
+
+  // Draw the emoji
+  textSize(256);
+  text(emoji, width / 2, height / 2);
+}
+
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // Store the label
+  label = results[0].label;
+}
